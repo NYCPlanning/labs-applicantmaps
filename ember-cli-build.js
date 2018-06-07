d38024ccf5dfd43a36159c408a4b4df8c93c29d3
Andy Cochran <acochran@planning.nyc.gov>
2018-06-07 15:38:35 -0400

    stub in a dropdown menu in labs-site-header

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    'ember-cli-foundation-6-sass': {
+      foundationJs: 'all',
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
