4dc99f5e668d2b2befcc4e3e23e976916369b1a8
allthesignals <wilburnforce@gmail.com>
2018-06-09 17:57:57 -0400

    Fix tests

--- tests/acceptance/user-can-fill-out-form-test.js
+++ tests/acceptance/user-can-fill-out-form-test.js
@@ -1,4 +1,4 @@
-import { module, skip } from 'qunit';
+import { module, test } from 'qunit';
 import { visit, currentURL, fillIn, click } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
@@ -7,13 +7,13 @@ module('Acceptance | user can fill out form', function(hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
 
-  skip('visiting /user-can-fill-out-form', async function(assert) {
+  test('user-can-fill-out-form', async function(assert) {
     server.createList('project', 10);
+    window.XMLHttpRequestFake = window.XMLHttpRequest;
     await visit('/projects/new');
     await fillIn('.project-name-field', 'ASDF');
     await fillIn('.applicant-name-field', 'ASDF');
     await click('.project-save-button');
-
 
     assert.equal(currentURL(), '/projects/11/edit');
   });
