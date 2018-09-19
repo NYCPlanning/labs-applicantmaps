521e149a88002cf6ccc1d29712fcdc1343716645
allthesignals <wilburnforce@gmail.com>
2018-09-19 11:43:06 -0400

    Setup polymorphic relationships bt applicant-maps

--- app/models/applicant-map.js
+++ app/models/applicant-map.js
@@ -3,6 +3,10 @@ import { attr, belongsTo } from '@ember-decorators/data';
 
 const { Model } = DS;
 
+/*
+  Abstract base class for applicant maps. Specific types
+  can be found under ./maps
+*/
 export default class ApplicantMapModel extends Model.extend({}) {
   // area, tax, zoning change, zoning section
   @attr('string') type;
