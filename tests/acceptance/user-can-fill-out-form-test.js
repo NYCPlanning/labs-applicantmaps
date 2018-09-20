9d60ece1b6ddeb6fbcc2937b4e952de2a55c062a
Hannah Kates <hannah.b.kates@gmail.com>
2018-09-20 15:13:42 -0400

    fixed trailing comma

--- tests/acceptance/user-can-fill-out-form-test.js
+++ tests/acceptance/user-can-fill-out-form-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import {
-  visit, currentURL, fillIn, click
+  visit, currentURL, fillIn, click,
 } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
