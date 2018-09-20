97ee6a59ac13a18a8185a1f64327e813b20de8b2
trbmcginnis <taylormcgin@gmail.com>
2018-09-20 17:24:40 -0400

    updating inset map

--- app/components/inset-map.js
+++ app/components/inset-map.js
@@ -1,7 +1,20 @@
 import Component from '@ember/component';
 import { argument } from '@ember-decorators/argument';
+import { action } from '@ember-decorators/object';
+import turfBbox from 'npm:@turf/bbox';
+import turfBuffer from 'npm:@turf/buffer';
 
 export default class extends Component {
   @argument
   boundsPolygon
+
+  @action
+  handleMapLoad(map) {
+    this.set('mapInstance', map);
+    const bounds = this.get('boundsPolygon');
+
+    map.fitBounds(turfBbox.default(turfBuffer.default(bounds, 3)), {
+      duration: 0,
+    });
+  }
 }
--- app/components/project-map-form.js
+++ app/components/project-map-form.js
@@ -12,7 +12,7 @@ export default class ProjectMapFormComponent extends Component {
           id: 'tax-lots',
           visible: true,
           layers: [
-            { tooltipable: true, tooltipTemplate: '{{address}}' },
+            { tooltipable: true, tooltipTemplate: '{{address}}; BBL: {{bbl}}' },
             {},
             { style: { layout: { 'text-field': '{numfloors}' } } },
           ],
