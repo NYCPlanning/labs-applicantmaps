0beb31a4b3b87519fece69c310a634b53199967c
allthesignals <wilburnforce@gmail.com>
2018-09-20 11:02:34 -0400

    Move static objects into constants

--- app/controllers/projects/edit/map/new.js
+++ app/controllers/projects/edit/map/new.js
@@ -1,50 +1,55 @@
 import Controller from '@ember/controller';
 import { action } from '@ember-decorators/object';
 import { service } from '@ember-decorators/service';
-
 import turfBbox from 'npm:@turf/bbox';
+
+const developmentSiteLayer = {
+  id: 'development-site-line',
+  type: 'line',
+  paint: {
+    'line-width': 2,
+    'line-color': 'red',
+  },
+};
+
+const projectAreaLayer = {
+  id: 'project-area-line',
+  type: 'line',
+  layout: {
+    visibility: 'visible',
+    'line-cap': 'round',
+  },
+  paint: {
+    'line-width': 6,
+    'line-dasharray': [
+      0.1,
+      2,
+    ],
+  },
+};
+
+const projectBufferLayer = {
+  id: 'project-buffer-line',
+  type: 'line',
+  paint: {
+    'line-color': 'rgba(116, 4, 80, 1)',
+    'line-width': 6,
+    'line-dasharray': [
+      0.5,
+      0.5,
+    ],
+  },
+};
 
 export default class NewProjectMapController extends Controller {
   @service
   notificationMessages;
 
-  developmentSiteLayer = {
-    id: 'development-site-line',
-    type: 'line',
-    paint: {
-      'line-width': 2,
-      'line-color': 'red',
-    },
-  }
+  developmentSiteLayer = developmentSiteLayer
 
-  projectAreaLayer = {
-    id: 'project-area-line',
-    type: 'line',
-    layout: {
-      visibility: 'visible',
-      'line-cap': 'round',
-    },
-    paint: {
-      'line-width': 6,
-      'line-dasharray': [
-        0.1,
-        2,
-      ],
-    },
-  }
+  projectAreaLayer = projectAreaLayer
 
-  projectBufferLayer = {
-    id: 'project-buffer-line',
-    type: 'line',
-    paint: {
-      'line-color': 'rgba(116, 4, 80, 1)',
-      'line-width': 6,
-      'line-dasharray': [
-        0.5,
-        0.5,
-      ],
-    },
-  }
+  projectBufferLayer = projectBufferLayer
 
   @action
   handleMapLoaded(map) {
