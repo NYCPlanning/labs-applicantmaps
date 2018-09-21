c8da00eda179a83f60dc6a3c419bc2b5a8c7a172
Hannah Kates <hannah.b.kates@gmail.com>
2018-09-21 14:39:23 -0400

    added WIP component for changing page size

---
+++ app/components/toggle-page-size.js
@@ -0,0 +1,20 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+	actions: {
+		toggleSize() {
+			this.
+		}
+	}
+});
+
+
+      // function toggleLandscape() {
+      //   var element = document.body;
+      //   element.classList.toggle('landscape');
+
+      //   // Trigger window resize to resize the map
+      //   var evt = document.createEvent('UIEvents');
+      //   evt.initUIEvent('resize', true, false, window, 0);
+      //   window.dispatchEvent(evt);
+      // }
