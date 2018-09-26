de12308c9817738c4c6657073775580bec139e89
allthesignals <wilburnforce@gmail.com>
2018-09-26 15:15:41 -0400

    Add search box back

--- tests/acceptance/user-can-search-test.js
+++ tests/acceptance/user-can-search-test.js
@@ -1,13 +1,14 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { visit, click, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | user can search', function(hooks) {
   setupApplicationTest(hooks);
 
-  test('visiting /user-can-search', async function(assert) {
-    await visit('/user-can-search');
+  test('User can start new project and search for something', async function(assert) {
+    await visit('/');
+    await 
 
-    assert.equal(currentURL(), '/user-can-search');
+    assert.equal(currentURL(), '/projects/new');
   });
 });
