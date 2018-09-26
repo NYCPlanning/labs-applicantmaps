c26ea5b54a4158bc78d2774fa6e11909191922ac
allthesignals <wilburnforce@gmail.com>
2018-09-26 12:08:35 -0400

    Fix linting error; skip useless tests

--- app/controllers/projects/edit/map/new.js
+++ app/controllers/projects/edit/map/new.js
@@ -1,7 +1,4 @@
 import Controller from '@ember/controller';
-import { action } from '@ember-decorators/object';
-import turfBbox from 'npm:@turf/bbox';
-
 import areaMapLegendConfig from '../../../../utils/area-map-legend-config';
 
 export default class NewProjectMapController extends Controller {
