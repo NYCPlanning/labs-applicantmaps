836aef174d8272a55d26f3182dc0e7ab1d0f8747
allthesignals <wilburnforce@gmail.com>
2018-10-02 15:04:50 -0400

    Install and configure some browsers for local testing only

--- testem.js
+++ testem.js
@@ -3,11 +3,16 @@
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
+  browser_start_timeout: 2000,
   launch_in_ci: [
     'Chrome',
   ],
   launch_in_dev: [
     'Chrome',
+    'BS_Firefox_Current',
+    'BS_Safari_Current',
+    'BS_MS_Edge',
+    'BS_IE_11',
   ],
   browser_args: {
     Chrome: {
@@ -21,6 +26,56 @@ module.exports = {
         '--remote-debugging-port=0',
         '--window-size=1440,900',
       ].filter(Boolean),
+    },
+  },
+  launchers: {
+    bs_edge: {
+      exe: 'node_modules/.bin/browserstack-launch',
+      args: ['--os', 'Windows', '--osv', '10', '--b', 'edge', '--bv', 'latest', '--u'],
+      protocol: 'browser',
+    },
+    bs_chrome: {
+      exe: 'node_modules/.bin/browserstack-launch',
+      args: ['--os', 'Windows', '--osv', '10', '--b', 'chrome', '--bv', 'latest', '-t', '1200', '--u'],
+      protocol: 'browser',
+    },
+    BS_Chrome_Current: {
+      exe: 'node_modules/.bin/browserstack-launch',
+      args: ['--os', 'Windows', '--osv', '10', '--b', 'chrome', '--bv', 'latest', '-t', '1200', '--u', '<url>'],
+      protocol: 'browser',
+    },
+    /* Chrome 41 for Googlebot as outlined by:
+     * https://developers.google.com/search/docs/guides/rendering
+     */
+    BS_Chrome_Googlebot: {
+      exe: 'node_modules/.bin/browserstack-launch',
+      args: ['--os', 'Windows', '--osv', '10', '--b', 'chrome', '--bv', '41.0', '-t', '1200', '--u', '<url>'],
+      protocol: 'browser',
+    },
+    BS_Firefox_Current: {
+      exe: 'node_modules/.bin/browserstack-launch',
+      args: ['--os', 'Windows', '--osv', '10', '--b', 'firefox', '--bv', 'latest', '-t', '1200', '--u', '<url>'],
+      protocol: 'browser',
+    },
+    BS_Safari_Current: {
+      exe: 'node_modules/.bin/browserstack-launch',
+      args: ['--os', 'OS X', '--osv', 'High Sierra', '--b', 'safari', '--bv', 'latest', '-t', '1200', '--u', '<url>'],
+      protocol: 'browser',
+    },
+    BS_Safari_Last: {
+      exe: 'node_modules/.bin/browserstack-launch',
+      args: ['--os', 'OS X', '--osv', 'Sierra', '--b', 'safari', '--bv', 'latest', '-t', '1200', '--u', '<url>'],
+      protocol: 'browser',
+    },
+    BS_MS_Edge: {
+      exe: 'node_modules/.bin/browserstack-launch',
+      args: ['--os', 'Windows', '--osv', '10', '--b', 'edge', '--bv', 'latest', '-t', '1200', '--u', '<url>'],
+      protocol: 'browser',
+    },
+    BS_IE_11: {
+      exe: 'node_modules/.bin/browserstack-launch',
+      args: ['--os', 'Windows', '--osv', '10', '--b', 'ie', '--bv', '11.0', '-t', '1500', '--u', '<url>'],
+      protocol: 'browser',
     },
   },
 };
