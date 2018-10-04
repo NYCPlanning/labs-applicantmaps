a22eeb818a91442a8fc77e11aa9e1796937cfd8e
trbmcginnis <taylormcgin@gmail.com>
2018-10-04 12:25:57 -0400

    Update click-add-different-project-map-types-test.js

--- tests/acceptance/click-add-different-project-map-types-test.js
+++ tests/acceptance/click-add-different-project-map-types-test.js
@@ -19,45 +19,7 @@ module('Acceptance | user can click to add different project map types', functio
   test('user can click to add different project map types', async (assert) => {
     server.createList('project', 10);
 
-    await visit('/projects/new');
-    await fillIn('.project-name-field', 'ASDF');
-    await fillIn('.applicant-name-field', 'ASDF');
-    await fillIn('.zap-project-id-field', 'ASDF');
-    await fillIn('.zap-project-description-field', 'Fill out description');
-
-    await mapboxGlLoaded();
-
-    await click('.draw-control-development-site');
-
-    await mapboxGlDrawReady();
-
-    await plotVerticies(
-      [
-        [550, 388],
-        [590, 444],
-        [604, 369],
-        [604, 369],
-      ],
-    );
-
-    await click('.draw-control-done');
-
-    await click('.draw-control-project-area');
-
-    await mapboxGlDrawReady();
-
-    await plotVerticies(
-      [
-        [550, 388],
-        [590, 444],
-        [604, 369],
-        [604, 369],
-      ],
-    );
-
-    await click('.draw-control-done');
-
-    await click('.project-save-button');
+    await visit('/projects/10');
     await click('.map-type-area-map');
 
     assert.equal(currentURL(), '/projects/11/edit/map/new?mapType=area-map');
