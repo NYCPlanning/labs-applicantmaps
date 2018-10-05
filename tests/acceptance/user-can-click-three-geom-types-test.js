a4abcd4282807956c33ad22bf36548fdda4900b3
Chris Whong <chris@whong.org>
2018-10-04 21:40:28 -0400

    add tests

--- tests/acceptance/user-can-click-three-geom-types-test.js
+++ tests/acceptance/user-can-click-three-geom-types-test.js
@@ -6,22 +6,44 @@ import {
 } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import mapboxGlLoaded from '../helpers/mapbox-gl-loaded';
+import mapboxGlDrawReady from '../helpers/mapbox-gl-draw-ready';
+import plotVerticies from '../helpers/plot-verticies';
 
 module('Acceptance | user can click three geom types', function(hooks) {
   setupApplicationTest(hooks);
 
-  test('User can click three geom types', async function(assert) {
+  test('User can click development site button', async function(assert) {
     await visit('/projects/new');
 
     await mapboxGlLoaded();
     await click('.draw-control-development-site');
     await click('.draw-control-cancel');
 
-    await click('.draw-control-project-area');
-    await click('.draw-control-cancel');
+    assert.equal(currentURL(), '/projects/new');
+  });
 
-    await click('.draw-control-rezoning-area');
-    await click('.draw-control-cancel');
+  test('Project area button is available only after user draws development site', async function(assert) {
+    await visit('/projects/new');
+
+    await mapboxGlLoaded();
+    await click('.draw-control-development-site');
+    // make sure .draw-control-project-area is not available
+    assert.dom('.draw-control-project-area').doesNotExist();
+
+    await mapboxGlDrawReady();
+
+    await plotVerticies(
+      [
+        [550, 388],
+        [590, 444],
+        [604, 369],
+        [604, 369],
+      ],
+    );
+
+    await click('.draw-control-done');
+
+    await click('.draw-control-project-area');
 
     assert.equal(currentURL(), '/projects/new');
   });
