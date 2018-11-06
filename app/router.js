d8d6c1eaefa6a90503bef2a43449cca6570383f3
Hannah Kates <hannah.b.kates@gmail.com>
2018-11-06 14:33:51 -0500

    fix link to projects.show route from rezoning page

--- app/router.js
+++ app/router.js
@@ -11,15 +11,15 @@ Router.map(function () { // eslint-disable-line
     this.route('show', { path: ':project_id' });
     this.route('new');
     this.route('edit', { path: ':project_id/edit' }, function () {
-      this.route('map', function () {
-        this.route('edit');
-      });
       this.route('steps', { path: '/' }, function() {
         this.route('development-site');
         this.route('project-area');
         this.route('rezoning');
       });
       this.route('geometry-edit');
+      this.route('map', function () {
+        this.route('edit');
+      });
     });
   });
 });
