b3dcf93b7b66e8900324592dc764ad7e58f9eedd
Hannah Kates <hannah.b.kates@gmail.com>
2018-11-14 17:27:58 -0500

    use mut value instead of action for new ember-inline-edit project properties

--- app/components/project-setup-status.js
+++ app/components/project-setup-status.js
@@ -1,29 +1,7 @@
 import Component from '@ember/component';
 import { argument } from '@ember-decorators/argument';
-import { action } from '@ember-decorators/object';
 
 export default class ProjectSetupStatus extends Component {
   @argument
   project;
-
-  @action
-  async updateProjectName() {
-    // const model = this.get('model');
-    // await model.save();
-    // this.get('notificationMessages').success('Project name updated!');
-  }
-
-  @action
-  async updateApplicantName() {
-    // const model = this.get('model');
-    // await model.save();
-    // this.get('notificationMessages').success('Applicant updated!');
-  }
-
-  @action
-  async updateZapProjectId() {
-    // const model = this.get('model');
-    // await model.save();
-    // this.get('notificationMessages').success('Project number updated!');
-  }
 }
