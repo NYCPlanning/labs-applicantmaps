a5a28d173fa26738b60566746f245849ad691dc2
Hannah Kates <hannah.b.kates@gmail.com>
2018-11-14 16:44:37 -0500

    move action to method on model

--- app/controllers/projects/edit/steps/rezoning.js
+++ app/controllers/projects/edit/steps/rezoning.js
@@ -4,9 +4,6 @@ import { action } from '@ember-decorators/object';
 export default class RezoningController extends Controller {
   @action
   setRezoningFalse() {
-    this.set('model.needRezoning', false);
-    this.set('model.needUnderlyingZoning', null);
-    this.set('model.needCommercialOverlay', null);
-    this.set('model.needSpecialDistrict', null);
+    this.get('model').setRezoningFalse();
   }
 }
