6b92f18d890fd87dce5cc7ba457f99d0a7a9dab2
allthesignals <wilburnforce@gmail.com>
2018-11-16 00:03:06 -0600

    rename project-map-form to project-geometries/layer-groups

--- tests/integration/components/project-geometries/layer-groups-test.js
+++ tests/integration/components/project-geometries/layer-groups-test.js
@@ -3,14 +3,14 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | project-map-form', function(hooks) {
+module('Integration | Component | project-geometries/layer-groups', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{project-map-form}}`);
+    await render(hbs`{{project-geometries/layer-groups}}`);
 
     assert.equal(this.element.textContent.trim(), '');
   });
