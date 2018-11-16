d805d1e0974a83968263bc907a7c8880f4840d8d
allthesignals <wilburnforce@gmail.com>
2018-11-15 23:13:01 -0600

    Fix tests

--- tests/acceptance/user-click-edit-new-url-test.js
+++ tests/acceptance/user-click-edit-new-url-test.js
@@ -3,6 +3,7 @@ import {
   visit,
   click,
   currentURL,
+  pauseTest,
 } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
@@ -16,7 +17,7 @@ module('Acceptance | user clicks edit project button and sees new url', function
 
     await visit('/projects/10');
     await click('[data-test-development-site]');
-    await click('[data-test-lots]');
+    await click('.tooltip [data-test-lots]');
 
     assert.equal(currentURL(), '/projects/10/edit/geometry-edit?mode=lots&type=development-site');
   });
