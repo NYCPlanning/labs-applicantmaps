33bc394a718729b1eba10a428d4c980d88e65f07
Chris Whong <chris@whong.org>
2018-11-21 18:14:35 -0500

    add more layers to “show” controller

--- app/controllers/projects/show.js
+++ app/controllers/projects/show.js
@@ -2,12 +2,27 @@ import Controller from '@ember/controller';
 import { action } from '@ember-decorators/object';
 import { developmentSiteLayer } from '../../components/project-geometries/development-site';
 import { projectAreaLayer } from '../../components/project-geometries/project-area';
+import { rezoningAreaLayer } from '../../components/project-geometries/rezoning-area';
+import { underlyingZoningLayer, underlyingZoningLabelsLayer } from '../../components/project-geometries/underlying-zoning';
+import { specialPurposeDistrictsLayer, specialPurposeDistrictsLabelsLayer } from '../../components/project-geometries/special-purpose-districts';
+
+
 import areaMapLegendConfig from '../../utils/area-map-legend-config';
 
-export default class NewProjectController extends Controller {
+export default class ShowProjectController extends Controller {
   projectAreaLayer = projectAreaLayer;
 
   developmentSiteLayer = developmentSiteLayer;
+
+  rezoningAreaLayer = rezoningAreaLayer;
+
+  underlyingZoningLayer = underlyingZoningLayer;
+
+  underlyingZoningLayer = underlyingZoningLabelsLayer
+
+  specialPurposeDistrictsLayer = specialPurposeDistrictsLayer
+
+  specialPurposeDistrictsLabelsLayer = specialPurposeDistrictsLabelsLayer
 
   areaMapLegendConfig = areaMapLegendConfig;
 
