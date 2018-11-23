493be041d077b3c72d80ce4e80a168623b9022b5
allthesignals <wilburnforce@gmail.com>
2018-11-23 15:09:04 -0500

    Rollback model if it’s dirty
    Rollback model if it’s dirty and user transitions away from geometry-edit

--- app/routes/projects/edit/geometry-edit.js
+++ app/routes/projects/edit/geometry-edit.js
@@ -1,4 +1,13 @@
 import Route from '@ember/routing/route';
+import { action } from '@ember-decorators/object';
 
-export default Route.extend({
-});
+export default class GeometryEditRoute extends Route {
+  @action
+  willTransition() {
+    const model = this.modelFor('projects.edit');
+
+    if (model.hasDirtyAttributes) {
+      model.rollbackAttributes();
+    }
+  }
+}
