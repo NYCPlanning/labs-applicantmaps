30a5fc5603c3763525c3c383be8a23d9127bae85
allthesignals <wilburnforce@gmail.com>
2018-11-23 20:27:15 -0500

    Re-add unify polygons test

--- tests/unit/utils/unify-polygons-test.js
+++ tests/unit/utils/unify-polygons-test.js
@@ -1,10 +1,31 @@
 import unifyPolygons from 'labs-applicant-maps/utils/unify-polygons';
-import { module, skip } from 'qunit';
+import { module, test } from 'qunit';
+
+const EmptyFeatureCollection = {
+  type: 'FeatureCollection',
+  features: [{
+    type: 'Feature',
+    geometry: null,
+    properties: {},
+  }],
+};
 
 module('Unit | Utility | unify-polygons', function() {
   // Replace this with your real tests.
-  skip('it works', function(assert) {
-    const result = unifyPolygons();
+  const dummyFeatureCollection = EmptyFeatureCollection;
+  const dummyPolygon = {
+    type: 'Polygon',
+    coordinates: [[0, 0], [0, 1], [1, 1], [1, 0]],
+  };
+
+  dummyFeatureCollection.features[0].geometry = dummyPolygon;
+  dummyFeatureCollection.features.push({
+    type: 'Feature',
+    geometry: dummyPolygon,
+  });
+
+  test('it works', function(assert) {
+    const result = unifyPolygons(dummyFeatureCollection);
     assert.ok(result);
   });
 });
