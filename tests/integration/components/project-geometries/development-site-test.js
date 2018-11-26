c19db73fa35a2209a76e66edd6f80f84227dec55
allthesignals <wilburnforce@gmail.com>
2018-11-26 15:55:54 -0500

    Test that button fades out

--- tests/integration/components/project-geometries/development-site-test.js
+++ tests/integration/components/project-geometries/development-site-test.js
@@ -1,21 +1,62 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, pauseTest, settled } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import { run } from '@ember/runloop';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+import mapboxGlLoaded from '../../../helpers/mapbox-gl-loaded';
+
+const DUMMY_FEATURE_COLLECTION = {
+  type: 'FeatureCollection',
+  features: [{
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [0, 0],
+    },
+  }],
+};
 
 module('Integration | Component | project-geometries/development-site', function(hooks) {
   setupRenderingTest(hooks);
+  setupMirage(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+    const store = this.owner.lookup('service:store');
+    const model = run(() => store.createRecord('project', {
+      projectName: 'Mulholland Drive',
+    }));
+
+    await model.save();
+
+    this.set('model', model);
 
     await render(hbs`
+      {{!-- EMBER WORMHOLE CONTAINER --}}
+      {{!-- The geometry type component will fill this div with markup once invoked --}}
+      <div id="geometry-type-draw-explainer">
+      </div>
+
+      {{!-- EMBER WORMHOLE CONTAINER --}}
+      <div id="geometry-type-save-box">
+      </div>
+
       {{#mapbox-gl as |map|}} 
-        {{project-geometries/development-site map=map}}
+        <div class="labs-map-loaded"></div>
+        {{project-geometries/development-site
+          mode='lots'
+          map=map
+          model=model}}
       {{/mapbox-gl}}
     `);
 
-    assert.equal(this.element.textContent.trim(), '');
+    await mapboxGlLoaded();
+
+    assert.equal(this.element.querySelector('[data-test-project-geometry-save]').disabled, true);
+
+    await model.set('developmentSite', DUMMY_FEATURE_COLLECTION);
+    await settled();
+
+    assert.equal(this.element.querySelector('[data-test-project-geometry-save]').disabled, false);
   });
 });
