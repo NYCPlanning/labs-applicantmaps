58c51997b5582123c69cbad7e9764d10973a3c9a
allthesignals <wilburnforce@gmail.com>
2018-11-28 13:53:53 -0500

    Add test to rezoning area, fix lint

--- tests/integration/components/project-geometries/rezoning-area-test.js
+++ tests/integration/components/project-geometries/rezoning-area-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, click, pauseTest } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import { run } from '@ember/runloop';
 import mapboxGlLoaded from '../../../helpers/mapbox-gl-loaded';
