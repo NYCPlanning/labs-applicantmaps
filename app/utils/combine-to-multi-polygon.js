5ee6eaddd002bc30c3ba3f835897cc90680ab073
allthesignals <wilburnforce@gmail.com>
2018-12-04 19:59:35 -0500

    Re-enable tests for utilities

--- app/utils/combine-to-multi-polygon.js
+++
@@ -1,5 +0,0 @@
-import turfUnion from '@turf/union';
-
-export default function unionFromFeatureCollection({ features }) {
-  return turfUnion(...features);
-}
--- tests/unit/utils/queries/rezoning-area-query-test.js
+++ tests/unit/utils/queries/rezoning-area-query-test.js
@@ -1,14 +1,23 @@
 import queriesRezoningAreaQuery from 'labs-applicant-maps/utils/queries/rezoning-area-query';
 import { module, test } from 'qunit';
-import random from '@turf/random';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+import { setupTest } from 'ember-qunit';
 
-const { randomPolygon } = random;
+module('Unit | Utility | queries/rezoning-area-query', function(hooks) {
+  setupTest(hooks);
+  setupMirage(hooks);
 
-module('Unit | Utility | queries/rezoning-area-query', function() {
   // Replace this with your real tests.
-  test('it works', function(assert) {
-    const zoningChange = randomPolygon(10);
-    const result = queriesRezoningAreaQuery(zoningChange);
+  test('it works', async function(assert) {
+    this.server.createList('project', 1);
+
+    const store = this.owner.lookup('service:store');
+    const model = await store.findRecord('project', 1);
+    await model.setDefaultUnderlyingZoning();
+
+    const result = queriesRezoningAreaQuery(
+      model.get('underlyingZoning'),
+    );
 
     assert.ok(result.features);
   });
