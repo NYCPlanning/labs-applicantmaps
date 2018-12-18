7a0cbd8767a974943488cbc436fc232eee5d832d
Chris Whong <chris@whong.org>
2018-12-18 15:10:58 -0500

    fix rezoning area calculation test

--- tests/acceptance/automatic-rezoning-area-test.js
+++ tests/acceptance/automatic-rezoning-area-test.js
@@ -14,31 +14,28 @@ module('Acceptance | Automated Rezoning Area Geometry', function(hooks) {
   setupMirage(hooks);
 
   test('After adding new feature to underlying zoning, rezoningArea has valid geometry', async function(assert) {
+    // create a dummy project without rezoning geometries
     this.server.create('project', { needsRezoning: true });
 
     const store = this.owner.lookup('service:store');
 
+    // visit the underlying zoning editing page
     await visit('/projects/1/edit/geometry-edit?mode=draw&type=underlying-zoning');
-
-    // save underlying zoning features (first save)
-    await click('[data-test-project-geometry-save]');
-
-    // go back to zoning district edit page
-    await visit('/projects/1/edit/geometry-edit?mode=draw&type=underlying-zoning');
-
     const model = store.peekRecord('project', 1);
+
     // rezoningArea should still be emptyDefault
     assert.equal(model.get('rezoningArea').features[0].properties.isEmptyDefault, true);
 
-    // adds a random polygon to the underlying zoning
+    // add a random polygon to the underlying zoning
     const underlyingZoning = model.get('underlyingZoning');
     underlyingZoning.features.push(randomPolygon(1).features[0]);
     model.set('underlyingZoning', underlyingZoning);
 
-    // save underlying zoning features (second save)
+    // save underlying zoning features
     await click('[data-test-project-geometry-save]');
 
     // rezoningArea should not have a null geom
+    // this confirms that that setRezoningArea diffed the zoning features and created a new polygon
     assert.notEqual(model.get('rezoningArea').features[0].geometry, null);
   });
 });
