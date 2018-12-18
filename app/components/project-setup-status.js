bbb653d3b8aa4960a10c855266bc8d7b81d68ff5
Hannah Kates <hannah.b.kates@gmail.com>
2018-12-18 16:05:19 -0500

    add share button to project-setup-status

--- app/components/project-setup-status.js
+++ app/components/project-setup-status.js
@@ -1,7 +1,22 @@
 import Component from '@ember/component';
+import { action } from '@ember-decorators/object';
 import { argument } from '@ember-decorators/argument';
+import { run } from '@ember/runloop';
 
 export default class ProjectSetupStatus extends Component {
   @argument
   project;
+
+  @action
+  handleShareSuccess() {
+    this.set('copySuccess', true);
+    run.later(() => {
+      this.set('copySuccess', false);
+    }, 2000);
+  }
+
+  @action
+  handleModalClose() {
+    this.set('copySuccess', false);
+  }
 }
