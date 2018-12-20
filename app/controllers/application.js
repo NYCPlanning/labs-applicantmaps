9f4c49b18cf0dd160b03aea781b62f9a482531e7
Hannah Kates <hannah.b.kates@gmail.com>
2018-12-20 17:13:27 -0500

    add custom draw style object array

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,13 +3,120 @@ import Controller from '@ember/controller';
 import mapboxgl from 'mapbox-gl';
 import MapboxDraw from 'mapbox-gl-draw';
 
+const drawStyles = [
+  // ACTIVE (being drawn)
+  // line stroke
+  {
+    id: 'gl-draw-line',
+    type: 'line',
+    filter: ['all', ['==', '$type', 'LineString'], ['!=', 'mode', 'static']],
+    layout: {
+      'line-cap': 'round',
+      'line-join': 'round',
+    },
+    paint: {
+      'line-color': '#D20C0C',
+      'line-dasharray': [0.2, 2],
+      'line-width': 2,
+    },
+  },
+  // polygon fill
+  {
+    id: 'gl-draw-polygon-fill',
+    type: 'fill',
+    filter: ['all', ['==', '$type', 'Polygon'], ['!=', 'mode', 'static']],
+    paint: {
+      'fill-color': '#D20C0C',
+      'fill-outline-color': '#D20C0C',
+      'fill-opacity': 0.1,
+    },
+  },
+  // polygon outline stroke
+  // This doesn't style the first edge of the polygon, which uses the line stroke styling instead
+  {
+    id: 'gl-draw-polygon-stroke-active',
+    type: 'line',
+    filter: ['all', ['==', '$type', 'Polygon'], ['!=', 'mode', 'static']],
+    layout: {
+      'line-cap': 'round',
+      'line-join': 'round',
+    },
+    paint: {
+      'line-color': '#D20C0C',
+      'line-dasharray': [0.2, 2],
+      'line-width': 2,
+    },
+  },
+  // vertex point halos
+  {
+    id: 'gl-draw-polygon-and-line-vertex-halo-active',
+    type: 'circle',
+    filter: ['all', ['==', 'meta', 'vertex'], ['==', '$type', 'Point'], ['!=', 'mode', 'static']],
+    paint: {
+      'circle-radius': 8,
+      'circle-color': '#FFF',
+    },
+  },
+  // vertex points
+  {
+    id: 'gl-draw-polygon-and-line-vertex-active',
+    type: 'circle',
+    filter: ['all', ['==', 'meta', 'vertex'], ['==', '$type', 'Point'], ['!=', 'mode', 'static']],
+    paint: {
+      'circle-radius': 6,
+      'circle-color': '#8a0808',
+    },
+  },
+
+  // INACTIVE (static, already drawn)
+  // line stroke
+  {
+    id: 'gl-draw-line-static',
+    type: 'line',
+    filter: ['all', ['==', '$type', 'LineString'], ['==', 'mode', 'static']],
+    layout: {
+      'line-cap': 'round',
+      'line-join': 'round',
+    },
+    paint: {
+      'line-color': '#000',
+      'line-width': 3,
+    },
+  },
+  // polygon fill
+  {
+    id: 'gl-draw-polygon-fill-static',
+    type: 'fill',
+    filter: ['all', ['==', '$type', 'Polygon'], ['==', 'mode', 'static']],
+    paint: {
+      'fill-color': '#000',
+      'fill-outline-color': '#000',
+      'fill-opacity': 0.1,
+    },
+  },
+  // polygon outline
+  {
+    id: 'gl-draw-polygon-stroke-static',
+    type: 'line',
+    filter: ['all', ['==', '$type', 'Polygon'], ['==', 'mode', 'static']],
+    layout: {
+      'line-cap': 'round',
+      'line-join': 'round',
+    },
+    paint: {
+      'line-color': '#000',
+      'line-width': 3,
+    },
+  },
+];
+
 const draw = new MapboxDraw({
   displayControlsDefault: false,
   controls: {
     polygon: true,
     trash: true,
   },
-  // styles: drawStyles, TODO modify default draw styles
+  styles: drawStyles,
 });
 
 export default class ApplicationController extends Controller {
