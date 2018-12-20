9a7cd2522296144cb52dc6326ef69b8e43b1e9a4
trbmcginnis <taylormcgin@gmail.com>
2018-12-20 17:23:23 -0500

    add application controller test

--- tests/unit/controllers/application-test.js
+++ tests/unit/controllers/application-test.js
@@ -1,12 +1,32 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import createMap from 'labs-applicant-maps/tests/helpers/create-map';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
 module('Unit | Controller | application', (hooks) => {
   setupTest(hooks);
+  setupMirage(hooks);
+
+  hooks.before(async function() {
+    this.map = await createMap();
+  });
+
+  hooks.after(function() {
+    this.map.remove();
+  });
 
   // Replace this with your real tests.
-  test('it exists', function (assert) {
+  test('Application Controller !', function (assert) {
+    this.server.create('project');
+    const { map } = this;
+
+    this.set('mapObject', {
+      mapInstance: map,
+    });
+
     const controller = this.owner.lookup('controller:application');
-    assert.ok(controller);
+
+    controller.send('handleMapLoad', map);
+    assert.ok(controller.get('mapInstance'));
   });
 });
