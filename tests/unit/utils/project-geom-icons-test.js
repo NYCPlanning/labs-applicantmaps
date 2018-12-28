f2e0312db51e21a2d67b14ad45cfa6b5845bd30f
Chris Whong <chris@whong.org>
2018-12-28 13:37:39 -0500

    write simple util test

--- tests/unit/utils/project-geom-icons-test.js
+++ tests/unit/utils/project-geom-icons-test.js
@@ -1,11 +1,9 @@
 import projectGeomIcons from 'labs-applicant-maps/utils/project-geom-icons';
 import { module, test } from 'qunit';
 
-module('Unit | Utility | project-geom-icons', function(hooks) {
-
-  // Replace this with your real tests.
-  test('it works', function(assert) {
-    let result = projectGeomIcons();
-    assert.ok(result);
+module('Unit | Utility | project-geom-icons', function() {
+  test('it exports an object with well-known properties', function(assert) {
+    const { developmentSiteIcon } = projectGeomIcons;
+    assert.ok(developmentSiteIcon);
   });
 });
