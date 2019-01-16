0d526014d3786350bbe9feb4a5e974ddf78d7cc4
allthesignals <wilburnforce@gmail.com>
2019-01-16 12:55:49 -0500

    Add test for annotations renderer

--- tests/integration/components/project-geometries/utils/annotations-renderer-test.js
+++ tests/integration/components/project-geometries/utils/annotations-renderer-test.js
@@ -3,6 +3,26 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
+const lineFeature = type => ({
+  type: 'Feature',
+  properties: {
+    'meta:mode': `draw_annotations:${type}`,
+  },
+  geometry: {
+    type: 'LineString',
+    coordinates: [
+      [
+        -72.49603271484375,
+        42.45183466943919,
+      ],
+      [
+        -71.949462890625,
+        42.64002037386321,
+      ],
+    ],
+  },
+});
+
 module('Integration | Component | project-geometries/utils/annotations-renderer', function(hooks) {
   setupRenderingTest(hooks);
 
@@ -10,7 +30,14 @@ module('Integration | Component | project-geometries/utils/annotations-renderer'
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    this.set('features', { type: 'FeatureCollection', features: [] });
+    this.set('features', {
+      type: 'FeatureCollection',
+      features: [
+        lineFeature('linear'),
+        lineFeature('curved'),
+        lineFeature('square'),
+      ],
+    });
 
     await render(hbs`{{project-geometries/utils/annotations-renderer annotations=features}}`);
 
