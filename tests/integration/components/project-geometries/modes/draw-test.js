002963a105825d68d6bd6d07b074d78bcdcb8997
allthesignals <wilburnforce@gmail.com>
2019-01-16 19:52:06 -0500

    Remove unused

--- tests/integration/components/project-geometries/modes/draw-test.js
+++ tests/integration/components/project-geometries/modes/draw-test.js
@@ -6,7 +6,6 @@ import {
   waitUntil,
   waitFor,
   typeIn,
-  pauseTest,
 } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
