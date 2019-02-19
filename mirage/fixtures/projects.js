9a8aa35e6774c2a6ac89bae034bad5a3e63c9a73
Andy Cochran <acochran@planning.nyc.gov>
2019-02-19 17:46:02 -0500

    add timestamp to area map; tweak area maps settings layout

--- mirage/fixtures/projects.js
+++ mirage/fixtures/projects.js
@@ -1,5 +1,40 @@
 export default [
   {
+    id: 12,
+    annotations: {
+      type: 'FeatureCollection',
+      features: [{
+        type: 'Feature',
+        properties: {
+          'meta:mode': 'draw_annotations:linear',
+          label: '29 ft',
+        },
+        geometry: {
+          coordinates: [
+            [
+              -73.91311260409391,
+              40.75817100752687,
+            ],
+            [
+              -73.91314440749528,
+              40.75808962172928,
+            ],
+          ],
+          type: 'LineString',
+        },
+      }],
+    },
+    projectName: 'test',
+    applicantName: 'test',
+    zapProjectId: 'test',
+    needProjectArea: false,
+    needRezoning: false,
+    needUnderlyingZoning: false,
+    needCommercialOverlay: false,
+    needSpecialDistrict: false,
+    geometricPropertyIds: [1, 2],
+  },
+  {
     id: 11,
     projectName: 'Test Project 9',
     applicantName: 'Firstname M. Longlastname',
