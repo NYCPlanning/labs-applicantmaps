b6e55c6555a04b1507688fb976b5bcda91cf6cec
jhogan <jhogan@planning.nyc.gov>
2019-02-21 11:25:34 -0500

    Add setters for project geometry layers to enable deletion

--- tests/unit/models/project-test.js
+++ tests/unit/models/project-test.js
@@ -1,7 +1,22 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 import { run } from '@ember/runloop';
+import { GEOMETRY_TYPES } from 'labs-applicant-maps/models/geometric-property';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+
+const NonEmptyFeatureCollection = {
+  type: 'FeatureCollection',
+  features: [{
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [0, 0],
+    },
+    properties: {
+      isEmptyDefault: true,
+    },
+  }],
+};
 
 const EmptyFeatureCollection = {
   type: 'FeatureCollection',
@@ -34,12 +49,7 @@ module('Unit | Model | project', (hooks) => {
 
   // Test for project completion when only development site is required
   test('it produces complete status', function (assert) {
-    const dummyFeatureCollection = EmptyFeatureCollection;
-
-    dummyFeatureCollection.features[0].geometry = {
-      type: 'Point',
-      coordinates: [0, 0],
-    };
+    const dummyFeatureCollection = NonEmptyFeatureCollection;
 
     const store = this.owner.lookup('service:store');
     const geometricProperty = store.createRecord('geometric-property', {
@@ -58,12 +68,7 @@ module('Unit | Model | project', (hooks) => {
 
   // Test for project incomplete project that hasn't answered rezoning question
   test('it produces rezoning step', function (assert) {
-    const dummyFeatureCollection = EmptyFeatureCollection;
-
-    dummyFeatureCollection.features[0].geometry = {
-      type: 'Point',
-      coordinates: [0, 0],
-    };
+    const dummyFeatureCollection = NonEmptyFeatureCollection;
 
     const store = this.owner.lookup('service:store');
     const geometricProperty = store.createRecord('geometric-property', {
@@ -78,5 +83,44 @@ module('Unit | Model | project', (hooks) => {
     }));
 
     assert.equal(model.get('currentStep.step'), 'rezoning');
+  });
+
+  // Test project geometric property setters (developmentSite)
+  test('it updates the given geometric property', function (assert) {
+    const dummyFeatureCollection = NonEmptyFeatureCollection;
+
+    const store = this.owner.lookup('service:store');
+
+    // create geometric property for each type
+    const geometricProperties = [];
+    GEOMETRY_TYPES.forEach((geometryType) => {
+      geometricProperties.push(store.createRecord('geometric-property', {
+        proposedGeometry: dummyFeatureCollection,
+        geometryType,
+      }));
+    });
+
+    // create project with all geometric properties
+    const model = run(() => store.createRecord('project', {
+      projectName: 'some project',
+      needProjectArea: false,
+      needRezoning: null,
+      geometricProperties,
+    }));
+
+    // assert values of all geometric properties are expected
+    GEOMETRY_TYPES.forEach((geometryType) => {
+      assert.equal(model.get(geometryType), dummyFeatureCollection);
+    });
+
+    // use setter to update values of all geometric properties
+    GEOMETRY_TYPES.forEach((geometryType) => {
+      model.set(geometryType, EmptyFeatureCollection);
+    });
+
+    // assert geometric properties have been updated
+    GEOMETRY_TYPES.forEach((geometryType) => {
+      assert.equal(model.get(geometryType), EmptyFeatureCollection);
+    });
   });
 });
