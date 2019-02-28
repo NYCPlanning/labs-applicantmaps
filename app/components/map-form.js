58c46ebe3f3f44e35ee0baf9f060c3c0ea5c775b
trbmcginnis <taylormcgin@gmail.com>
2019-02-28 13:30:25 -0500

    83: only display ground-level building footprint outlines

--- app/components/map-form.js
+++ app/components/map-form.js
@@ -47,21 +47,6 @@ const defaultLayerGroups = {
         }, // subway_entrances_labels
       ],
     },
-    {
-      id: 'building-footprints',
-      visible: true,
-      layers: [
-        {
-          style: {
-            paint: {
-              'fill-opacity': 0.35,
-              'fill-color': 'rgba(33, 35, 38, 0)',
-              'fill-outline-color': 'rgba(33, 35, 38, 0.8)',
-            },
-          },
-        },
-      ],
-    },
     { id: 'special-purpose-districts', visible: false },
     {
       id: 'citymap',
@@ -198,6 +183,7 @@ export default class MapFormComponent extends Component {
 
     store.query('layer-group', query).then((layerGroups) => {
       const { meta } = layerGroups;
+
       const sources = store.peekAll('source').toArray().uniqBy('meta.description');
 
       this.set('mapConfiguration', {
@@ -277,6 +263,7 @@ export default class MapFormComponent extends Component {
     const scaleControl = new mapboxgl.ScaleControl({ maxWidth: 200, unit: 'imperial' });
     map.addControl(scaleControl, 'bottom-left');
 
+    // for editing the OpenStreetMap basemap style (look at maputnik, mapbox GL JS API docs, or style.json in the layers-api for reference)
     const basemapLayersToHide = [
       'background',
       'highway_path',
@@ -301,6 +288,11 @@ export default class MapFormComponent extends Component {
       'railway_dashline',
     ];
     basemapLayersToHide.forEach(layer => map.removeLayer(layer));
+
+    // editing building layer in the basemap to only show shape outlines and filtering for only ground floor structures
+    map.setPaintProperty('building', 'fill-color', 'rgba(234, 234, 229, 0)');
+    map.setPaintProperty('building', 'fill-outline-color', 'rgba(0, 0, 0, 1)');
+    map.setFilter('building', ['==', 'render_min_height', 0]);
   }
 
   @action
