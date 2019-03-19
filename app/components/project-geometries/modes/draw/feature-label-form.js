08981b99dc2671ab3f4f9a7b8dac10de599b1300
trbmcginnis <taylormcgin@gmail.com>
2019-03-19 14:02:01 -0400

    411: allow users to add new zoning dist labels (#482)

--- app/components/project-geometries/modes/draw/feature-label-form.js
+++ app/components/project-geometries/modes/draw/feature-label-form.js
@@ -29,6 +29,9 @@ export default class FeatureLabelFormComponent extends Component {
   @argument
   options=null;
 
+  @argument
+  canAddNewDistricts=true;
+
   isBold = false;
 
   isLarge = false;
@@ -53,6 +56,27 @@ export default class FeatureLabelFormComponent extends Component {
       this.updateSelectedFeature('textFont', 'bold');
     }
     this.drawStateCallback();
+  }
+
+  // action to hide the "Add" option in power-select-with-create if current search input matches options
+  @action
+  hideCreateOptionIfAlreadyExists(input) {
+    const options = this.get('options'); // array of district options for power-select
+    // make input upper case so that still matches when user types in lower case
+    const upperCaseInput = input.toUpperCase();
+    // array of districts that match substring currently being typed into search box
+    const searchOptions = options.filter(d => d.includes(upperCaseInput));
+    // array of all elements that match between options array and searchOptions array
+    const matchArray = options.filter(function(item) {
+      return searchOptions.includes(item);
+    });
+    // canAddNewDistricts is used to exclude commercial overlays, where we do not want users to add new zoning districts
+    const canAddNewDistricts = this.get('canAddNewDistricts');
+    // if matchArray has no elements AND noNewDistricts is false, return true and show "Add"
+    if (matchArray.length < 1 && canAddNewDistricts === true) {
+      return true;
+    }
+    return false;
   }
 
   @action
