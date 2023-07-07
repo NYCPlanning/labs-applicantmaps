b66248402a9b540517a38acd00b40a9b72c4fb82
Bryan Marchena <bmarchena@planning.nyc.gov>
2023-07-07 10:34:16 -0400

    v3.10.1...v3.12.1

--- testem.js
+++ testem.js
@@ -11,9 +11,12 @@ module.exports = {
   browser_args: {
     Chrome: {
       ci: [
-        '--no-sandbox',
-        '--disable-gpu',
+        // --no-sandbox is needed when running Chrome inside a container
+        process.env.CI ? '--no-sandbox' : null,
         '--headless',
+        '--disable-dev-shm-usage',
+        '--disable-software-rasterizer',
+        '--mute-audio',
         '--remote-debugging-port=0',
         '--window-size=1440,900',
       ].filter(Boolean),
